5049c1c7ab9bf699edbb03de9cec53e718b7499b
HocVuVan <van.hocvu@mission.plus>
2021-06-16 12:46:01 +0700

    add chart

--- src/views/Classrooms/ClassroomActivity.js
+++ src/views/Classrooms/ClassroomActivity.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
+import { Pie } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
+
 import {
   Table,
   Button,
@@ -32,6 +35,7 @@ export const ClassroomActivity = (props) => {
   const [activities, setActivities] = useState([]);
   const [isFetching, setFetching] = useState(false);
   const [showFormChangeDeadline, setShowFormChangeDeadline] = useState(false);
+  const [showAnalysis, setShowAnalysis] = useState(false);
 
   const [formChangeDeadline] = Form.useForm();
   const { id } = useParams();
@@ -121,6 +125,56 @@ export const ClassroomActivity = (props) => {
       console.log(error);
     }
   };
+  const dataPieChart = {
+    labels: ["Chưa làm bài", "Hoàn thành"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [2, 1],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+  const data = {
+    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+    datasets: [
+      {
+        label: "Số học sinh",
+        data: [1, 3, 2, 8, 10, 16, 2, 3, 0, 1],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
   const columns = [
     {
       title: "Tên hoạt động",
@@ -195,6 +249,7 @@ export const ClassroomActivity = (props) => {
             <div style={{ display: "flex", justifyContent: "space-around" }}>
               <Tooltip title="Thống kê kết quả">
                 <AreaChartOutlined
+                  onClick={() => setShowAnalysis(true)}
                   style={{ fontSize: 16, color: "#008DF2", marginRight: 10 }}
                 />
               </Tooltip>
@@ -324,6 +379,22 @@ export const ClassroomActivity = (props) => {
             />
           </Form.Item>
         </Form>
+      </Modal>
+      <Modal
+        width={660}
+        title="Thống kê kết quả"
+        onCancel={() => setShowAnalysis(false)}
+        onOk={() => setShowAnalysis(false)}
+        visible={showAnalysis}
+      >
+        <Row align="bottom" justify="space-between">
+          <Col>
+            <Pie data={dataPieChart} />
+          </Col>
+          <Col>
+            <Bar data={data} options={options} />
+          </Col>
+        </Row>
       </Modal>
     </>
   );
